6e5790b76763b143b5e87ea7eb00ad19b9f68c30
Bek <begmurod1999@mail.ru>
2019-12-03 23:23:33 +0500

    Создание приложения для клиента и сервера

---
+++ Client/Client.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        private IPEndPoint IpPoint { get; set; }
+        public bool Work { get; set; }
+        private Socket Socket { get; set; }
+
+        public Client(int port, string address)
+        {
+            IpPoint = new IPEndPoint(IPAddress.Parse(address), port);          
+        }
+
+        public void Start()
+        {
+            Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket.Connect(IpPoint);
+            Console.WriteLine(DateTime.Now +  " Соединение установленно.");
+            Work = true;
+
+            Task processingSend = new Task(ProcessingSend);
+            processingSend.Start();
+
+            Task processingReceive = new Task(ProcessingReceive);
+            processingReceive.Start();
+        }
+
+        public void Stop()
+        {
+            Socket.Close();
+            Work = false;
+        }
+
+        private void ProcessingSend()
+        {
+            try
+            {
+                while (Work)
+                {
+                    Socket.Send(BitConverter.GetBytes(Convert.ToDouble(new Random().Next(20, 30)*0.99)));
+                    Console.WriteLine(DateTime.Now +  " Запрос отправлен.");
+                    Thread.Sleep(2700);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Work = false;
+            }
+
+        }
+
+        private void ProcessingReceive()
+        {
+            try
+            {
+                while (Work)
+                {
+                    byte[] answer = new byte[100];
+                    Socket.Receive(answer, answer.Length, SocketFlags.None);
+
+                    Console.WriteLine(DateTime.Now +  " Полученное число: " + BitConverter.ToInt32(answer,0));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Work = false;
+            }
+        }
+    }
+}
---
+++ practice_1/user.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace practice_1
+{
+    class User
+    {
+        private Socket Socket { get; set; }
+        private Server Server;
+        private int Id { get; set; }
+        private int Tasks = 0;
+
+        public User(Server Server, Socket Socket, int Id)
+        {
+            this.Socket = Socket;
+            this.Server = Server;
+            this.Id = Id;
+        }
+
+        public void Processing()
+        {
+            try
+            {
+                while (Server.Work)
+                {
+                    double number = Reading();
+
+                    if (Tasks == 0)
+                    {
+                        Tasks = 1;
+                        Task task = new Task(() => Working(number));
+                        task.Start();
+                    }
+                    else
+                    {
+                        Tasks++;
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(DateTime.Now + " Пользователь: " + Id +" Ошибка: " + e.Message);
+            }
+
+            Exit();
+        }
+
+        private double Reading()
+        {
+            byte[] receiveBuffer = new byte[8];
+            Socket.Receive(receiveBuffer, receiveBuffer.Length, SocketFlags.None);
+            Console.WriteLine(DateTime.Now +  " Пользователь: " + Id + " Запрос получен."+ BitConverter.ToDouble(receiveBuffer, 0));
+            return Math.Round(BitConverter.ToDouble(receiveBuffer, 0));
+        }
+
+        private void Working(double number)
+        {
+            Thread.Sleep(5000);
+
+            Answer(BitConverter.GetBytes((int)number));
+        }
+
+        private void Answer(byte[] answer)
+        {
+            for (int i = 0; i < Tasks; i++)
+            {
+                Socket.Send(answer);
+                Console.WriteLine(DateTime.Now+ " Пользователь " + Id + " отправлен ответ на запрос.");
+            }
+            Tasks = 0;
+        }
+
+        private void Exit()
+        {
+            Socket.Close();
+        }
+        
+    }
+}
